90df266f51df4ce2258a4a6770c11e3ae4fe03f8
Diogo Pedro <220000891@esg.ipsantarem.pt>
2026-05-06 11:08:45 +0100

    Atualização do Passageiro e MyQueue e Criação da Paragem

--- src/models/Paragem.java
+++ src/models/Paragem.java
@@ -1,5 +1,48 @@
 package models;
 
+import structures.MyQueue;
+
 public class Paragem {
-    
+
+    private String nome;
+    private MyQueue filaPassageiros;
+    private Paragem next;
+
+    // Construtor
+    public Paragem(String nome) {
+        this.nome = nome;
+        this.filaPassageiros = new MyQueue();
+        this.next = null;
+    }
+
+    // --- Getters e Setters ---
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public MyQueue getFilaPassageiros() {
+        return filaPassageiros;
+    }
+
+    public void setFilaPassageiros(MyQueue filaPassageiros) {
+        this.filaPassageiros = filaPassageiros;
+    }
+
+    public Paragem getNext() {
+        return next;
+    }
+
+    public void setNext(Paragem next) {
+        this.next = next;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Paragem: " + nome + " [" + filaPassageiros.size() + " passageiro(s) em espera]";
+    }
 }
--- src/models/Passageiro.java
+++ src/models/Passageiro.java
@@ -1,27 +1,25 @@
 package models;
 
 public class Passageiro {
-
+    /*idade e deficiência (Perguntar à porfessora)*/
     private String nome;
-
+    
     // Construtor
     public Passageiro(String nome) {
         this.nome = nome;
     }
 
-    // Getter para obter o nome
+    // --- Getter e Setter --
     public String getNome() {
         return nome;
     }
 
-    // Setter para alterar o nome (se for preciso no futuro)
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    // Facilita a impressão
     @Override
     public String toString() {
-        return this.nome;
+        return nome;
     }
 }
