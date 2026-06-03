fc75c5ae9a7cd763344fc30850a2f5122a8acdf3
Diogo Pedro <220000891@esg.ipsantarem.pt>
2026-06-03 17:53:35 +0100

    Atualização do README e dos Models (Autocarro, Paragem e Passageiro)

--- src/models/Passageiro.java
+++ src/models/Passageiro.java
@@ -1,14 +1,11 @@
 package models;
 
 public class Passageiro {
-    /*idade e deficiência (Perguntar à porfessora)*/
     private String nome;
-    private String destino; // Nome da paragem de destino do passageiro
     
     // Construtor
-    public Passageiro(String nome, String destino) {
+    public Passageiro(String nome) {
         this.nome = nome;
-        this.destino = destino;
     }
 
     // --- Getters e Setters ---
@@ -20,16 +17,8 @@ public class Passageiro {
         this.nome = nome;
     }
 
-    public String getDestino() {
-        return destino;
-    }
-
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
-
     @Override
     public String toString() {
-         return nome + " -> " + destino;
+         return nome;
     }
 }
